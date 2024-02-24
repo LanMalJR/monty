7884e220d4df143d9966efb3efacee68c7fefc25
LanMalJR <lanmaljrlm@gmail.com>
2024-02-24 14:49:59 +0200

    main.c

--- main.c
+++ main.c
@@ -1,26 +1,20 @@
+#define _GNU_SOURCE 
 #include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-/**
- * main - Monty bytecode interpreter.
- * @argc: Number of arguments.
- * @argv: Arguments.
- *
- * Return: EXIT_SUCCESS or EXIT_FAILURE.
- */
 int main(int argc, char **argv)
 {
     stack_t *stack = NULL;
     char *line = NULL, *token = NULL;
-    size_t len = 0;
-    size_t read;
-    unsigned int line_number = 1;
+    size_t len =  0;
+    ssize_t read; // Corrected type for read
+    unsigned int line_number =  1;
     FILE *file;
 
-    if (argc != 2)
+    if (argc !=  2)
     {
         fprintf(stderr, "USAGE: monty file\n");
         return (EXIT_FAILURE);
@@ -33,26 +27,31 @@ int main(int argc, char **argv)
         return (EXIT_FAILURE);
     }
 
-    while ((read = getline(&line, &len, file)) != -1)
+    while ((read = getline(&line, &len, file)) != -1) 
     {
         line_number++;
         token = strtok(line, " \t\n");
 
-        if (token == NULL || strlen(token) == 0)
+        if (token == NULL || strlen(token) ==  0)
             continue;
 
-        if (strcmp(token, "push") == 0)
+        if (strcmp(token, "push") ==  0)
         {
             token = strtok(NULL, " \t\n");
             if (token == NULL || isspace((unsigned char)*token))
             {
                 fprintf(stderr, "L%u: usage: push integer\n", line_number);
-                free_stack(stack);
+                free_stack(stack); 
                 fclose(file);
                 free(line);
                 return (EXIT_FAILURE);
             }
-
         }
     }
-}            
+
+    free_stack(stack); 
+    fclose(file);
+    free(line);
+    return (EXIT_SUCCESS); 
+}
+           
